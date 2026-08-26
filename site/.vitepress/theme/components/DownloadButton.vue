e44592d7f02504a6e380d78dd2b2450658021692
Xianpeng Shen <xianpeng.shen@gmail.com>
2026-08-26 04:03:59 +0300

    Stop the SPA router from turning the DMG download link into a 404 (#77)

--- site/.vitepress/theme/components/DownloadButton.vue
+++ site/.vitepress/theme/components/DownloadButton.vue
@@ -31,7 +31,10 @@ onMounted(async () => {
 </script>
 
 <template>
-  <a v-if="dmgURL" class="kh-btn kh-btn-primary" :href="dmgURL">
+  <!-- `download` keeps VitePress's SPA router from intercepting the click:
+       .dmg is not in its known-extensions list, so without it the router
+       rewrites the href to …dmg.html and lands on the 404 page. -->
+  <a v-if="dmgURL" class="kh-btn kh-btn-primary" :href="dmgURL" download>
     {{ label }}<span v-if="version" class="kh-btn-version">v{{ version }}</span>
   </a>
   <a v-else class="kh-btn kh-btn-primary" :href="fallbackHref">{{ fallbackLabel }}</a>
